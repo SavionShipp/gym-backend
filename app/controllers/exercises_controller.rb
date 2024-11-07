add8c8d0f1fea80b557a1510555894eeac8c0d66
SavionShipp <shippsavion209@yahoo.com>
2024-11-06 19:30:11 -0800

    added update action

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -26,4 +26,15 @@ class ExercisesController < ApplicationController
     render json: {message: "exercise has been deleted successfully"}
   end
 
+  def update 
+    @exercise = Exercise.find_by_id(params[:id])
+    @exercise.update(
+      name: params[:name] || @exercise.name,
+      description: params[:description] || @exercise.description,
+      image_url: params[:image_url] || @exercise.image_url,
+      video_url: params[:video_url] || @exercise.video_url
+    )
+    render :show
+  end
+
 end
