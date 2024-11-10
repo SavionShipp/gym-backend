cd7cb6ac21613f3ab32a1f8e7ecd4fa066f35f3d
SavionShipp <shippsavion209@yahoo.com>
2024-11-10 08:25:08 -0800

    added user auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(
+    user = User.new(
       name: params[:name],
       email: params[:email],
-      image_url: params[:image_url],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
     )
-    render :show
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
   end
 
   def destroy
@@ -31,6 +36,7 @@ class UsersController < ApplicationController
       name: params[:name] || @user.name,
       email: params[:email] || @user.email,
       image_url: params[:image_url] || @user.image_url,
+      password: params[:password] || @user.password
     )
     render :show
   end
