8f3cc13d92e149c9c094eab6ae87920c28bf50c7
SavionShipp <shippsavion209@yahoo.com>
2024-11-10 08:44:20 -0800

    added routine actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,6 @@
 
 # Exercise.create(name:"bicep curls", description: "curl the dumbbell towards your shoulder from a rested position", image_url:"https://fitnessprogramer.com/wp-content/uploads/2021/02/Dumbbell-Curl.gif", video_url:"https://www.youtube.com/watch?v=ykJmrZ5v0Oo")
 
-User.create(name:"Savion", email:"shippsavion209@yahoo.com", image_url:"https://image.maxpreps.io/Gallery/JZ0FW6A-DkqHj4LapHwzsA/A-pjy6zb40uFM_HYKxqdBA/1,660,520/st_mary%27s_vs_garces_memorial_(fr_barry_christmas_classic)_boys_basketball_image.jpg")
+# User.create(name:"Savion", email:"shippsavion209@yahoo.com", image_url:"https://image.maxpreps.io/Gallery/JZ0FW6A-DkqHj4LapHwzsA/A-pjy6zb40uFM_HYKxqdBA/1,660,520/st_mary%27s_vs_garces_memorial_(fr_barry_christmas_classic)_boys_basketball_image.jpg")
+
+Routine.create(user_id: 1, exercise_id: 1, reps: 5, sets: 3)
